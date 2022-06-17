5dac30dfe49268e99c869cf7a4935f1ffbd33361
Amar <kumaramar35@gmail.com>
2022-06-17 14:51:36 +0530

    Birthday parties

--- utils/birthdayWishes.js
+++ utils/birthdayWishes.js
@@ -1,5 +1,5 @@
  const messages = [
-    {"value":"May you be gifted with life’s biggest joys and never-ending bliss. After all, you yourself are a gift to earth, so you deserve the best!"},
+    {"value":"May you be gifted with life’s biggest joys and never-ending bliss. After all, you yourself  are a gift to earth, so you deserve the best!"},
     {"value":"Count not the candles…see the lights they give. Count not the years, but the life you live. Wishing you a wonderful time ahead!" }, 
     {"value":"Forget the past; look forward to the future, for the best things are yet to come!"},
     {"value":"You are only young once, but you can be immature for a lifetime!"},
